0a3cc836980a134336d55484a7ae15b82987ed86
LAPTOP-5SG2CNP0\NGUYEN <thainguyen122004@gmail.com>
2024-01-10 20:05:18 +0700

    delete social login

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -76,15 +76,6 @@ const Signup = () => {
                             </form>
                         </div>
                     </div>
-
-                    {/* social login */}
-                    <div>
-                        <hr />
-                        <div className="flex w-full items-center flex-col mt-5 gap-3">
-                            <button onClick={handleRegister} className='block'> <img src={googleLogo} alt="" className='w-12 h-12 inline-block' />Log in with Google</button>
-                            <button> <img src={fbLogo} alt="" className='w-10 h-10 inline-block mr-1' />Log in with Facebook</button>
-                        </div>
-                    </div>
                 </div>
             </div>
         </div>
