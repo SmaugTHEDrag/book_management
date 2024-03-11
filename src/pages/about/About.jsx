d82ff5a7947171e447de4d5651654976f3be7cd1
LAPTOP-5SG2CNP0\NGUYEN <thainguyen122004@gmail.com>
2024-03-11 18:49:22 +0700

    change the avatar of our teammate

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -27,28 +27,28 @@ const About = () => (
               <TeamMember
                 name="Phạm Thái Nguyên"
                 job="Front-end, Back-end and Database"
-                imageSrc="https://cdn.discordapp.com/attachments/1051127040834805864/1194644770237206639/280661399_684201749476230_7457983245617736127_n.png?ex=65b11aa4&is=659ea5a4&hm=40fefb1e04c501b350b325f29db4ec36642190825c11351351c12a9a65840fd2&"
+                imageSrc="https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1566425108i/33.jpg"
               />
               {/* Repeat the TeamMember component for other team members */}
               <TeamMember
                 name="Lê Thành Trung"
                 job="Front-end, UI and Tester"
-                imageSrc="https://cdn.discordapp.com/attachments/1175297910066393090/1194692533742211182/IMG_5182.jpg?ex=65ba81a0&is=65a80ca0&hm=554545e28b65faecc2d56a6aa66d1c644d2e19f1750a29ec6a7226de43cd6e68&"
+                imageSrc="https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1400602609i/28187.jpg"
               />
               <TeamMember
                 name="Văn Tân Phú"
                 job="Front-end, UI and Tester"
-                imageSrc="https://cdn.discordapp.com/attachments/1175297910066393090/1194645011892031488/274633992_1298808633921546_3459297813985372380_n.png?ex=65b11ade&is=659ea5de&hm=c3f77113507b01dbce9ec5108854f691cc3253f7e17c525f0fd4a5dcc840bf35&"
+                imageSrc="https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1661032875i/11127.jpg"
               />
               <TeamMember
                 name="Sử Tiến Long"
                 job="Figma and Tester"
-                imageSrc="https://cdn.discordapp.com/attachments/1051127040834805864/1194105457585958932/367993760_176917412075744_7693660866132411014_n.jpg?ex=65af245e&is=659caf5e&hm=99c8a86cc862b8001c12d271f8f84e9b6626ca760c75ec37b826481e6c2c9681&"
+                imageSrc="https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1534298934i/862041.jpg"
               />
               <TeamMember
                 name="Lê Văn Hùng"
                 job="Figma and Tester"
-                imageSrc="https://cdn.discordapp.com/attachments/1081069042212798545/1194627484092747776/IMG_9623.png?ex=65b10a8b&is=659e958b&hm=d1e056bc7489a3496eb335fcc04b79b43d2ba7cb2550089bdd8cb8c568723a65&"
+                imageSrc="https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1567840212i/10572.jpg"
               />
             </ul>
           </div>
